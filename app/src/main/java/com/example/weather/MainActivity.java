8f8721d560722fc07a7440dc8a3efa0468e4e0c6
Swanee Prakash <swanee.prakash38@gmail.com>
2023-05-17 23:11:22 +0530

    commit

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -121,5 +121,6 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onProviderEnabled(String provider) {
                 }
+
             }
         }
