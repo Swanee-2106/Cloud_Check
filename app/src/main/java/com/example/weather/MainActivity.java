ae99c324af32b4b895eeffba8ee488ce13da36f6
Swanee Prakash <swanee.prakash38@gmail.com>
2023-05-17 22:41:06 +0530

    commit

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -104,7 +104,9 @@ public class MainActivity extends AppCompatActivity {
     }
     private void getWeatherForCurrentLocation() {
         mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mLocationListner = new LocationListener() {
 
-    }
+
+        }
 
 
