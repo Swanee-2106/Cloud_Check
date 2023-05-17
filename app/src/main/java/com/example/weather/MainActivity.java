2da8e1b7fde6be8797033b46e27458f21b5a10ef
Swanee Prakash <swanee.prakash38@gmail.com>
2023-05-17 22:59:21 +0530

    commit

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -119,7 +119,5 @@ public class MainActivity extends AppCompatActivity {
                 public void onStatusChanged(String provider, int status, Bundle extras) {
                 }
 
-
-
             }
         }
